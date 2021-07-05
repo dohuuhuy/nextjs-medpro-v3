25c4262e9f00f3324eebdd549a5b40ac1b457789
Do Huu Huy <dohuuhuy2@gmail.com>
2021-07-05 18:31:07 +0700

    css

--- src/components/page/home/index.tsx
+++ src/components/page/home/index.tsx
@@ -1,14 +1,17 @@
 import { Button } from 'antd'
 import React from 'react'
 import styles from './style.module.less'
+import css from './style.module.css'
 import { ExampleComponent } from '@tntran496/ts-ant-demo'
+// import './../../../../node_modules/@tntran496/ts-ant-demo/libs/index.css'
+import '@tntran496/ts-ant-demo/libs/index.css'
 
 const HomeDetail = () => {
   return (
     <>
-      <ExampleComponent text={'helo nhe alo alo hihi'} />
+      <ExampleComponent text={'helo monday'} />
       <div className={styles.main}>helo</div>
-      <Button>huyi l</Button>
+      <Button className={css.btnDemo}>huyi </Button>
     </>
   )
 }
