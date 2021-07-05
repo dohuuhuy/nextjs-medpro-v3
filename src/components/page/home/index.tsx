873356c6955e7ab5f36e62051cf4337e1f9d9fc1
Do Huu Huy <dohuuhuy2@gmail.com>
2021-07-05 11:20:11 +0700

    demo

--- src/components/page/home/index.tsx
+++ src/components/page/home/index.tsx
@@ -1,13 +1,12 @@
 import { Button } from 'antd'
 import React from 'react'
 import styles from './style.module.less'
-import '@medpro/booking-libs/lib/index.css'
-import { Header } from '@medpro/booking-libs'
+import { ExampleComponent } from '@tntran496/ts-ant-demo'
 
 const HomeDetail = () => {
   return (
     <>
-      <Header />
+      <ExampleComponent text={'helo nhe alo alo hihi'} />
       <div className={styles.main}>helo</div>
       <Button>huyi</Button>
     </>
