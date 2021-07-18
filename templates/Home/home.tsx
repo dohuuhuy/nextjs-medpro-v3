da53f50f07af90f9af91c303c4377b9a62fb9aa4
Do Huu Huy <dohuuhuy2@gmail.com>
2021-07-18 20:00:25 +0700

    Btn_local_hospital

--- templates/Home/home.tsx
+++ templates/Home/home.tsx
@@ -1,17 +1,21 @@
 import { Layout } from 'antd'
 import dynamic from 'next/dynamic'
-import React from 'react'
+import React, { ReactNode } from 'react'
 import styles from './styles.module.less'
 
 const Header = dynamic(() => import('@components/organisms/Header'))
 const Download = dynamic(() => import('@components/organisms/DownloadApp'))
 const Footer = dynamic(() => import('@components/organisms/Footer'))
 
-const HomeLayout = () => {
+type Props = {
+  children?: ReactNode
+}
+const HomeLayout = ({ children }: Props) => {
   return (
     <Layout className={styles.layout}>
       <Header />
       <Download />
+      {children}
       <Footer />
     </Layout>
   )
