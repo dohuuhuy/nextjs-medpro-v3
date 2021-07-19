66da540f28da77acd0e3e1c9df7d14dd49dc2159
Do Huu Huy <dohuuhuy2@gmail.com>
2021-07-19 15:06:32 +0700

    huy

--- templates/Home/home.tsx
+++ templates/Home/home.tsx
@@ -3,9 +3,9 @@ import dynamic from 'next/dynamic'
 import React, { ReactNode } from 'react'
 import styles from './styles.module.less'
 
-const Header = dynamic(() => import('@components/organisms/Header'))
+// const Header = dynamic(() => import('@components/organisms/Header'))
 const Download = dynamic(() => import('@components/organisms/DownloadApp'))
-const Footer = dynamic(() => import('@components/organisms/Footer'))
+// const Footer = dynamic(() => import('@components/organisms/Footer'))
 
 type Props = {
   children?: ReactNode
@@ -13,10 +13,10 @@ type Props = {
 const HomeLayout = ({ children }: Props) => {
   return (
     <Layout className={styles.layout}>
-      <Header />
+      {/* <Header /> */}
       <Download />
       {children}
-      <Footer />
+      {/* <Footer /> */}
     </Layout>
   )
 }
