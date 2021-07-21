61da2232d7bcd517603cf8fb12dfa202e6772d4e
Do Huu Huy <dohuuhuy2@gmail.com>
2021-07-21 14:19:08 +0700

    MenuHeader

--- src/components/test/HeaderCustom/MenuHeader.tsx
+++ src/components/test/HeaderCustom/MenuHeader.tsx
@@ -1,36 +1,23 @@
-import React, { useState } from 'react'
-import style from './styles.module.less'
 import Link from 'next/link'
+import React from 'react'
 import { ItemMenu } from './header.interface'
+import style from './styles.module.less'
 
 interface Props {
   Menu: Array<ItemMenu>
 }
 
 const MenuHeader = ({ Menu }: Props) => {
-  const [itemMenu, setItemMenu] = useState(Menu)
-
-  function setOnClickMenu(order: number) {
-    let arr = itemMenu?.map((item) => {
-      if (order === item.sortOrder) {
-        item.active = true
-      } else {
-        item.active = false
-      }
-      return { ...item }
-    })
-    setItemMenu(arr)
-  }
-
   return (
     <ul className={style.navbar_nav}>
-      {itemMenu?.map((item) => (
+      {Menu?.map((item) => (
         <li
           className={item.active ? style.active : style.nav_item}
           key={item.key}
-          onClick={() => setOnClickMenu(item.sortOrder)}
         >
-          <Link href={item.link}>{item.name}</Link>
+          <Link href={item.link || '/'}>
+            <a>{item.label}</a>
+          </Link>
         </li>
       ))}
     </ul>
--- src/components/test/HeaderCustom/header.interface.ts
+++ src/components/test/HeaderCustom/header.interface.ts
@@ -1,59 +1,60 @@
-export interface PropsHeader{
-    dataHeader: Array<Item> | any;
-    Authencartion: Array<Info> | any;
+export interface PropsHeader {
+  dataHeader: Array<Item> | any
+  Authencartion: Array<Info> | any
 }
 
 export interface Info {
-    isAuthen: boolean,
-    nameUser?: string
+  isAuthen: boolean
+  nameUser?: string
 }
 
 export interface Item {
-    logoHeader: string;
-    logoMobile: string;
-    menu: Array<ItemMenu>;
-    menuMobile: Array<ItemMenuMobile>;
-    listSupport: Array<ItemDrawer>;
-    support: Array<ItemSupport>;
+  logoHeader: string
+  logoMobile: string
+  menu: Array<ItemMenu>
+  menuMobile: Array<ItemMenuMobile>
+  listSupport: Array<ItemDrawer>
+  support: Array<ItemSupport>
 }
 
 export interface ItemMenu {
-    id?: string;
-    key: string;
-    link: string;
-    url?: string;
-    name: string;
-    content: string;
-    sortOrder: number;
-    icon: string;
-    active: boolean;
+  id?: string
+  key: string
+  link: string
+  url?: string
+  label: string
+  content: string
+  sortOrder: number
+  icon: string
+  active: boolean
+  group: string
 }
 
 export interface ItemMenuMobile {
-    id?: string;
-    key: string;
-    link: string;
-    url?: string;
-    label: string;
-    group?: string;
-    sortOrder: number;
-    icon: string;
+  id?: string
+  key: string
+  link: string
+  url?: string
+  label: string
+  group?: string
+  sortOrder: number
+  icon: string
 }
 
 export interface ItemSupport {
-    textSuport: string;
-    icon: string;
-    phone: string;
+  textSuport: string
+  icon: string
+  phone: string
 }
 
 export interface ItemDrawer {
-    funcGroup: Array<ItemButton>;
-    guideGroup: Array<ItemButton>;
-    supportGroup: Array<ItemButton>;
+  funcGroup: Array<ItemButton>
+  guideGroup: Array<ItemButton>
+  supportGroup: Array<ItemButton>
 }
 
-export interface ItemButton{
-    icon: string;
-    label: string;
-    link: string;
+export interface ItemButton {
+  icon: string
+  label: string
+  link: string
 }
