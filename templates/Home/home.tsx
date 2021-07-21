4f587ec32f763b68d6ded21f6b787ffd3a931e24
Do Huu Huy <dohuuhuy2@gmail.com>
2021-07-21 10:12:18 +0700

    HomeLayout

--- templates/Home/home.tsx
+++ templates/Home/home.tsx
@@ -1,51 +1,24 @@
-import { openInNewTab } from '@components/atoms/openInNewTab'
-import { Demo } from '@n17dccn172/booking-libs'
-// import { Demo } from '@components/test/Demo'
 import { Layout } from 'antd'
 import dynamic from 'next/dynamic'
 import React, { ReactNode } from 'react'
 import styles from './styles.module.less'
 
-// const Header = dynamic(() => import('@components/organisms/Header'))
+const Introduce = dynamic(() => import('@components/organisms/IntroduceApp'))
 const Download = dynamic(() => import('@components/organisms/DownloadApp'))
-// const BannerHome = dynamic(() => import('@components/organisms/BannerHomeApp'))
-// const SliderHospital = dynamic(
-//   () => import('@components/organisms/SilderHospital')
-// )
 const SupportMethod = dynamic(
   () => import('@components/organisms/SupportMethod')
 )
-const Introduce = dynamic(() => import('@components/organisms/IntroduceApp'))
 const Footer = dynamic(() => import('@components/organisms/Footer'))
-// const FooterCustom = dynamic(() => import('@components/test/FooterCustom'))
 
 type Props = {
   children?: ReactNode
 }
-const HomeLayout = ({}: Props) => {
-  const handerFuncTest = (name: string) => {
-    alert(name)
-  }
 
+const HomeLayout = ({}: Props) => {
   return (
     <Layout className={styles.layout}>
-      {/* <Header /> */}
-      {/* <BannerHome />
-      <SliderHospital /> */}
       <Introduce />
-
-      {/* <Demo
-        text={'viết bằng typescript function và có sử dụng module.less'}
-        funcTest={() => handerFuncTest('huyi')}
-        funcLogin={() =>
-          openInNewTab('https://id-v121.medpro.com.vn/check-phone')
-        }
-      />
-
-      {/* <Introduce /> */}
       <Download />
-
-      {/* {children} */}
       <SupportMethod />
       <Footer />
     </Layout>
