d0bb354f361d48726f443e69dd90d0f78544ee9f
Do Huu Huy <dohuuhuy2@gmail.com>
2021-08-17 10:40:52 +0700

    VNĐ

--- test/components/ThongTinDatKham/utils/func.ts
+++ test/components/ThongTinDatKham/utils/func.ts
@@ -54,3 +54,12 @@ export const handlerValue = (props: any, KEY: any) => {
       break
   }
 }
+
+export const VNĐ = (x = 0) => {
+  if (x) {
+    const parts = x.toString().split('.')
+    parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, '.')
+    return parts.join('.')
+  }
+  return 0
+}
