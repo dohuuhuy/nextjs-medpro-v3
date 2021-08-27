727801e8163aac51a31e227b5de95e29974e8375
HaiSon1007 <sonphan.ittc@gmail.com>
2021-08-27 13:49:26 +0700

    comit

--- test/components/ThongTinHoSoCustom/PhieuKhamBenh/index.tsx
+++ test/components/ThongTinHoSoCustom/PhieuKhamBenh/index.tsx
@@ -1,8 +1,8 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './styles.module.less'
 import { DownOutlined, UpOutlined } from '@ant-design/icons'
 import { HandleFilter } from './components/func'
-
+import { AnyAaaaRecord } from 'dns'
 export interface Medthods {
   listUser: any[]
 }
@@ -10,30 +10,41 @@ export interface Medthods {
 
 export const PhieuKhamBenh = (Props: Medthods) => {
   const Filter = HandleFilter(Props)
-  console.log("Filter ", Filter)
+  const [show, setishow] = useState(false)
+
+  const onShow = (index: any) => {
+    console.log("index ", index)
+    setishow(show !== index ? index : '')
+  }
+  const x = GetURLParameter("testing.medpro.com.vn:80")
+  function GetURLParameter(sParam: any) {
+    const text = sParam.split(":")
+    return text[0]
+  }
+  console.log(x)
   return (
     <div className={styles.Container}>
       <h1>Danh sách phiếu khám bệnh</h1>
       <ul className={styles.listUser}>
-        {Filter.map((listUser: any, i: any) => {
-          // const isShowCardFooter = show === index ? '' : styles.hidden
-          console.log('item', listUser[0], listUser[1])
+        {Filter.map((listUser: any, index: any) => {
           return (
-            <li key={i + "d"}>
-              <div className={styles.cardHeader}>
-                <p>{listUser[0]}</p>
-                <DownOutlined />
+            <li key={index + "d"}>
+              <div className={styles.cardHeader} onClick={() => onShow(index)}>
+                <span>{listUser[0]}</span>
+                {show === index ? <UpOutlined /> : <DownOutlined />}
               </div>
-              <ul className={styles.listExam}>
-                {listUser[1].map((item: any, i: any) => {
+              <ul className={show === index ? styles.listExam : styles.hidden}>
+                {listUser[1].map((listUserItem: any, i: any) => {
                   return (
                     <li key={i + "d"}>
-                      <ul>
-                        {item.map(({ key, value }: any, i: any) => {
+                      <ul className={styles.listExamItem}>
+                        {listUserItem.map(({ key, value }: any, i: any) => {
                           return (
                             <li key={i + "d"}>
-                              <div>{key}</div>
-                              <div>{value}</div>
+                              <div className={styles.cardView}>
+                                <div>{key}</div>
+                                <div>{value}</div>
+                              </div>
                             </li>
                           )
                         })}
