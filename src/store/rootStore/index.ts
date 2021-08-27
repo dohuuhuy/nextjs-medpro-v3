8b8023e815465177a536269a1657854587eb1b92
Do Huu Huy <dohuuhuy2@gmail.com>
2021-08-27 12:29:43 +0700

    createWrapper

--- src/store/rootStore/index.ts
+++ src/store/rootStore/index.ts
@@ -3,11 +3,11 @@ import { rootReducer } from '@store/rootReducer'
 import rootSaga from '@store/rootSaga'
 import { createWrapper } from 'next-redux-wrapper'
 import { createStore, Store } from 'redux'
-import { Persistor, persistStore } from 'redux-persist'
+import { persistStore } from 'redux-persist'
 import { Task } from 'redux-saga'
 import { bindMiddleware, sagaMiddleware } from './handlerStore'
 import { persistedReducer } from './persistConfig'
-export let persistor: Persistor
+
 export interface SagaStore extends Store<AppState> {
   sagaTask?: Task | undefined
 }
@@ -21,11 +21,12 @@ export const store = () => {
     store = createStore(rootReducer as any, bindMiddleware([sagaMiddleware]))
     store.sagaTask = sagaMiddleware.run(rootSaga)
     return store
+  } else {
+    store = createStore(persistedReducer(), bindMiddleware([sagaMiddleware]))
+    store.__persistor = persistStore(store)
+    store.sagaTask = sagaMiddleware.run(rootSaga)
+    return store
   }
-  store = createStore(persistedReducer(), bindMiddleware([sagaMiddleware]))
-  store.__persistor = persistStore(store)
-  store.sagaTask = sagaMiddleware.run(rootSaga)
-  return store
 }
 
 export const wrapper = createWrapper<SagaStore>(store)
