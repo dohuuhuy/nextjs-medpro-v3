087aad85a94531b847f6d4b90fe4e0f5a1860a53
Do Huu Huy <dohuuhuy2@gmail.com>
2021-09-04 12:02:26 +0700

    dùng fetch

--- src/containers/index.ts
+++ src/containers/index.ts
@@ -1,4 +1,3 @@
-import { getData } from '@store/api'
 import { JSON_EXP } from 'json mẫu/bvtest'
 
 export const indexContainer = async (ctx: any) => {
@@ -17,14 +16,31 @@ export const indexContainer = async (ctx: any) => {
 export const getListPartners = async () => {
   const url =
     'https://resource-testing.medpro.com.vn/static/list-partner/listPartner.json'
-  const rs = await getData(url)
+  const rs = await apiGet(url)
 
   return rs
 }
 
 export const findPartnerId = ({ listPartners, host }: any) => {
   const domain = host?.slice(0, host.indexOf(':'))
-  const res: any = listPartners.find((i: any) => i.domain.includes(domain))
+  const res: any = listPartners?.find((i: any) => i.domain.includes(domain))
   if (!res) return 'medpro'
   return res?.partnerId
+}
+
+export const apiGet = async (url: any) => {
+  const option: any = {
+    headers: {
+      'Cache-Control': 'no-cache, no-store, must-revalidate',
+      Pragma: 'no-cache',
+      Expires: 0
+    }
+  }
+  return await fetch(url, option)
+    .then((rs) => {
+      return rs ? rs.json() : null
+    })
+    .catch(() => {
+      return null
+    })
 }
