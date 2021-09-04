1d6c39e2f4ff26cae0ff74ee5239d655c52f6c83
Do Huu Huy <dohuuhuy2@gmail.com>
2021-09-04 16:58:11 +0700

    xaxc nhan thong tin

--- pages/chon-benh-vien.tsx
+++ pages/chon-benh-vien.tsx
@@ -1,16 +1,24 @@
 import SelectHospitalPage from '@components/pages/SelectHospitalPage'
 import dynamic from 'next/dynamic'
 import { useEffect } from 'react'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { SelectHospitalCtl } from 'src/containers/SelectHosital'
 import * as ac from '@actionStore/rootAction'
+import { AppState } from '@store/interface'
 const DefaultLayout = dynamic(() => import('@templates/Default'))
 
 const ChonBenhVienPage = (props: any) => {
   const dispatch = useDispatch()
+  const listHospital = useSelector(
+    (state: AppState) => state.hospitalReducer.listHospital
+  )
+  const listCity = useSelector(
+    (state: AppState) => state.totalDataReducer.listCity
+  )
+
   useEffect(() => {
-    dispatch(ac.getListCity())
-    dispatch(ac.getListHospital())
+    !listCity && dispatch(ac.getListCity())
+    !listHospital && dispatch(ac.getListHospital())
   }, [])
 
   return <SelectHospitalPage {...props.data} />
