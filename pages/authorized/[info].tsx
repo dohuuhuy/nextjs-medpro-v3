964fe1a61235ed6694b4b8adecf018a7257dabba
Do Huu Huy <dohuuhuy2@gmail.com>
2021-09-10 14:26:45 +0700

    login

--- pages/authorized/[info].tsx
+++ pages/authorized/[info].tsx
@@ -23,9 +23,9 @@ const Author = () => {
   const user = useSelector((state: AppState) => state.userReducer)
 
   useEffect(() => {
-    router.push('/')
     if (check(user?.userInfo?.token)) {
       dispatch(ac.UserLogin(query))
+      router.push('/')
     }
   })
 
