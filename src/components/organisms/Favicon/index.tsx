d811db4078bfece725c77630a3939676a757166a
Do Huu Huy <dohuuhuy2@gmail.com>
2021-09-12 17:22:39 +0700

    head

--- src/components/organisms/Favicon/index.tsx
+++ src/components/organisms/Favicon/index.tsx
@@ -1,7 +1,14 @@
 import React from 'react'
 
 const FavIcon = () => {
-  return <link rel='shortcut icon' href='/favicon/medpro.png' />
+  return (
+    <>
+      <link rel='shortcut icon' href='/favicon/medpro.png' />
+      <link rel='apple-touch-icon' href='/favicon/medpro.png' />
+      <link rel='manifest' href='/manifest.json' />
+      <link rel='manifest' href='/manifest.json' />
+    </>
+  )
 }
 
 export default FavIcon
