3183534448695dfa52cd35494070350b75e52d61
Do Huu Huy <dohuuhuy2@gmail.com>
2021-09-13 00:37:41 +0700

    getListPartners

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 git stash 
 git fetch 
 git pull 
+yarn
 yarn build:testing 
 pm2 restart web-srr
--- src/containers/app.ts
+++ src/containers/app.ts
@@ -1,4 +1,3 @@
-import api from 'store/api'
 import { findPartnerId } from '@utils/run_local_hospitals'
 import { JSON_EXP } from 'json mẫu/bvtest'
 
@@ -18,7 +17,7 @@ export const appCtrl = async (ctx: any) => {
 export const getListPartners = async () => {
   const url =
     'https://resource-testing.medpro.com.vn/static/list-partner/listPartner.json'
-  const rs = await api(url)
+  const rs = await fetch(url)
 
-  return rs.data
+  return await rs.json()
 }
