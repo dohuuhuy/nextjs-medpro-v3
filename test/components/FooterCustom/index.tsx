78ae1e98aecf704e1964a49933e4bcf698dc3ee2
Do Huu Huy <dohuuhuy2@gmail.com>
2021-09-15 15:50:25 +0700

    FooterCustom

--- test/components/FooterCustom/index.tsx
+++ test/components/FooterCustom/index.tsx
@@ -1,5 +1,26 @@
+import Container from '@componentsTest/Container'
+import { Row, Col } from 'antd'
 import React from 'react'
 
 export const FooterCustom = () => {
-  return <div>hello</div>
+  return (
+    <Container>
+      <Row>
+        <Col></Col>
+      </Row>
+      <Row>
+        <Col>
+          <p>Medpro - Giải pháp tiếp cận y tế thông minh</p>
+        </Col>
+      </Row>
+      <Row>
+        <Col></Col>
+      </Row>
+      <Row>
+        <Col>
+          <p>@2021 Medpro Copyright. All Rights Reserved.</p>
+        </Col>
+      </Row>
+    </Container>
+  )
 }
