a657481d048ad70b82fcd69c6cbace3fe2dd4d41
Do Huu Huy <dohuuhuy2@gmail.com>
2021-09-17 12:45:04 +0700

    icons

--- test/components/Icon/index.tsx
+++ test/components/Icon/index.tsx
@@ -1,11 +1,11 @@
-/* eslint-disable @next/next/no-img-element */
-import * as React from 'react'
-import * as icons from './icons'
+import React from 'react'
+import Image from 'next/image'
 import styles from './styles.module.less'
 import cx from 'classnames'
+import * as icons from './icons'
 export type Icons = typeof icons
 export type IconName = keyof Icons
-export type IconSize = '20' | '40' | '60' | '80'
+export type IconSize = '20' | '25' | '30' | '35' | '40' | '50' | '60' | '80'
 
 interface IconProps {
   name: IconName
@@ -14,12 +14,18 @@ interface IconProps {
 }
 
 export const Icon = ({ name, size = '20', className }: IconProps) => {
+  const Icon = icons[name]
   return (
-    <img
-      className={cx(styles.icon, className)}
-      src={icons[name]}
-      alt='icon'
-      style={{ width: size + 'px', height: size + 'px' }}
-    />
+    <span className={cx(styles.icon)}>
+      <Image
+        src={Icon}
+        width={size}
+        height={size}
+        alt=''
+        className={cx(className)}
+      />
+    </span>
   )
 }
+
+//  chổ này , cân cấu hinh lại , svg , cái nay mới nhận dạng image thôi
