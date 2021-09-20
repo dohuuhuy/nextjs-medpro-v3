e9e5e62942f7f6fd16bc1b6f8d59f2dcc7d19145
HaiSon1007 <sonphan.ittc@gmail.com>
2021-09-20 17:04:10 +0700

    comit new

--- test/components/DatKham/ListDatKham/Specialist/index.tsx
+++ test/components/DatKham/ListDatKham/Specialist/index.tsx
@@ -6,17 +6,19 @@ import { Special, dataSpecial } from './utils/data'
 
 export const SpecialContent = () => {
   return (
-    <ul className={styles.listCard}>
-      {dataSpecial.map((item: Special) => {
-        return (
-          <li key={uniqueId()}>
-            <div className={styles.cardContent}>
-              <Icon name={item.icon} size="35" />
-              <p>{item.name}</p>
-            </div>
-          </li>
-        )
-      })}
-    </ul>
+    <div className={styles.container}>
+      <ul className={styles.listCard}>
+        {dataSpecial.map((item: Special) => {
+          return (
+            <li key={uniqueId()}>
+              <div className={styles.cardContent}>
+                <Icon name={item.icon} size="35" />
+                <p>{item.name}</p>
+              </div>
+            </li>
+          )
+        })}
+      </ul>
+    </div>
   )
 }
