2eb4c452c7422b5501ee0ff8b8f89c18f6087197
Do Huu Huy <dohuuhuy2@gmail.com>
2021-09-21 12:03:31 +0700

    chon bênh viện

--- test/components/Icon/icons/index.ts
+++ test/components/Icon/icons/index.ts
@@ -1,3 +1,4 @@
 export { default as cskh } from './cskh.svg'
 export { default as thongbao } from './thongbao.svg'
 export { default as timkiem } from './timkiem.svg'
+export { default as yeuthich } from './yeuthich.svg'
--- test/components/Icon/index.tsx
+++ test/components/Icon/index.tsx
@@ -4,7 +4,16 @@ import styles from './styles.module.less'
 
 export type Icons = typeof icons
 export type IconName = keyof Icons
-export type IconSize = '20' | '25' | '30' | '35' | '40' | '50' | '60' | '80'
+export type IconSize =
+  | '15'
+  | '20'
+  | '25'
+  | '30'
+  | '35'
+  | '40'
+  | '50'
+  | '60'
+  | '80'
 
 interface IconProps {
   name: IconName
