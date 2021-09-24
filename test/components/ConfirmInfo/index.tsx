cfab7d9bf43fd7db411dc9537724ce315c3e08f6
Do Huu Huy <dohuuhuy2@gmail.com>
2021-09-24 11:07:23 +0700

    ConfirmInfo

--- test/components/ConfirmInfo/index.tsx
+++ test/components/ConfirmInfo/index.tsx
@@ -7,54 +7,7 @@ import { CloseCircleOutlined } from '@ant-design/icons'
 import { uniqueId } from 'lodash'
 import { Icon } from '../Icon'
 
-
 export const ConfirmInfo = () => {
-  const HandleModile = (text: string) => {
-    const str1 = text.slice(0, 4)
-    const str2 = text.slice(4, 7)
-    const str3 = text.slice(7, 10)
-    return str1.concat(" " + str2 + " " + str3)
-  }
-  const Profile = [
-    {
-      title: '',
-      value: 'Huỳnh Ngọc Toàn (Bạn)'
-    },
-    {
-      title: 'Giới tính:',
-      value: 'Nam'
-    },
-    {
-      title: 'Ngày sinh:',
-      value: '24/12/2021'
-    },
-    {
-      title: 'Số điện thoại:',
-      value: HandleModile('0903232223')
-    },
-  ]
-  const Info = [
-    {
-      title: 'Hình thức khám:',
-      value: 'Dịch vụ'
-    },
-    {
-      title: 'Bác sĩ:',
-      value: 'Nguyễn Văn A'
-    },
-    {
-      title: 'Chuyên khoa:',
-      value: 'Răng Hàm Mặt'
-    },
-    {
-      title: 'Ngày khám:',
-      value: '12/12/2022'
-    },
-    {
-      title: 'Giờ khám:',
-      value: '07:30'
-    },
-  ]
   return (
     <Container className={styles.container}>
       <Row className={styles.rowInfo}>
@@ -65,13 +18,19 @@ export const ConfirmInfo = () => {
             <ul className={styles.listViewProfile}>
               <li>
                 <div className={styles.viewProfile}>
-                  <Button className={styles.btnProfile} icon={<Icon name="plus" size='20' />} />
+                  <Button
+                    className={styles.btnProfile}
+                    icon={<Icon name='plus' size='20' />}
+                  />
                   <p>Tạo hồ sơ</p>
                 </div>
               </li>
               <li>
                 <div className={styles.viewProfile}>
-                  <Button className={styles.btnProfile} icon={<Icon name="bacsinam" size='55' />} />
+                  <Button
+                    className={styles.btnProfile}
+                    icon={<Icon name='bacsinam' size='55' />}
+                  />
                   <p>Minh Anh</p>
                 </div>
               </li>
@@ -82,7 +41,10 @@ export const ConfirmInfo = () => {
                   return (
                     <li key={uniqueId()}>
                       <div className={styles.cardBody}>
-                        <p>{title}<span>{value}</span></p>
+                        <p>
+                          <span>{title}</span>
+                          <span>{value}</span>
+                        </p>
                       </div>
                     </li>
                   )
@@ -98,17 +60,18 @@ export const ConfirmInfo = () => {
                   return (
                     <li key={uniqueId()}>
                       <div className={styles.cardBody}>
-                        <p>{title}<span>{value}</span></p>
+                        <p>
+                          {title}
+                          <span>{value}</span>
+                        </p>
                       </div>
                     </li>
                   )
                 })}
                 <CloseCircleOutlined />
               </ul>
-
             </div>
           </div>
-
         </Col>
         <Col xl={8}>
           <CardFee />
@@ -117,3 +80,50 @@ export const ConfirmInfo = () => {
     </Container>
   )
 }
+
+const HandleModile = (text: string) => {
+  const str1 = text.slice(0, 4)
+  const str2 = text.slice(4, 7)
+  const str3 = text.slice(7, 10)
+  return str1.concat(' ' + str2 + ' ' + str3)
+}
+const Profile = [
+  {
+    title: '',
+    value: 'Huỳnh Ngọc Toàn (Bạn)'
+  },
+  {
+    title: 'Giới tính:',
+    value: 'Nam'
+  },
+  {
+    title: 'Ngày sinh:',
+    value: '24/12/2021'
+  },
+  {
+    title: 'Số điện thoại:',
+    value: HandleModile('0903232223')
+  }
+]
+const Info = [
+  {
+    title: 'Hình thức khám:',
+    value: 'Dịch vụ'
+  },
+  {
+    title: 'Bác sĩ:',
+    value: 'Nguyễn Văn A'
+  },
+  {
+    title: 'Chuyên khoa:',
+    value: 'Răng Hàm Mặt'
+  },
+  {
+    title: 'Ngày khám:',
+    value: '12/12/2022'
+  },
+  {
+    title: 'Giờ khám:',
+    value: '07:30'
+  }
+]
