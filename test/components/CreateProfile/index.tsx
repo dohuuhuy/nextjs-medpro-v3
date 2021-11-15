49ba6cf18cb0d1a7002c5b7354b02cc51ffb5f8d
HaiSon1007 <sonphan.ittc@gmail.com>
2021-11-15 18:26:04 +0700

    comit

---
+++ test/components/CreateProfile/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+// import { useForm } from 'react-hook-form'
+import Container from '@componentsTest/Container'
+
+function CreateForm() {
+  return (
+    <Container>
+      <form>
+        <p>abc</p>
+      </form>
+    </Container>
+  )
+}
+
+export default CreateForm
