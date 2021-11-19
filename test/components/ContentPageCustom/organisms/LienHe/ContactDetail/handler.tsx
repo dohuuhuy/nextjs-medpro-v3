622af1e0813e448c1d0bb25a399863afc818fc1b
HaiSon1007 <sonphan.ittc@gmail.com>
2021-11-19 16:52:21 +0700

    update > fix > medpro

--- test/components/ContentPageCustom/organisms/LienHe/ContactDetail/handler.tsx
+++ test/components/ContentPageCustom/organisms/LienHe/ContactDetail/handler.tsx
@@ -1,22 +1,31 @@
-import * as yup from 'yup'
+// import * as yup from 'yup'
+import {z} from 'zod'
 
 const phoneRegExp = /(84|0[3|5|7|8|9])+([0-9]{8})\b/g
 
-export const schemaContact = yup.object().shape({
-  fullname: yup
-    .string()
-    .trim('Vui lòng không nhập khoảng trắng !')
-    .strict(true)
-    .required('Vui lòng nhập họ tên !'),
-  email: yup.string().email('Vui nhập đúng định dạng !'),
-  phone: yup
-    .string()
-    .matches(phoneRegExp, 'Số điện thoại không dúng định dạng !')
-    .typeError('Vui lòng nhập số !')
-    .required('Vui lòng nhập số điện thoại !'),
-  problem: yup.string().required('Vui lòng chọn vấn đề!'),
-  content: yup.string().required('Vui lòng nhập nội dung !')
-})
+export const schemaContact = z.object({
+  fullname: z.string().nonempty("Vui lòng nhập họ tên !"),
+  email: z.string().email("Vui nhập đúng định dạng email !"),
+  phone: z.string().nonempty("Vui lòng nhập số điện thoại !").regex(phoneRegExp, 'Số điện thoại không dúng định dạng !'),
+  problem: z.string().nonempty("Vui lòng chọn vấn đề !"),
+  content: z.string().nonempty("Vui lòng nhập nội dung cần trợ giúp !")
+
+});
+// export const schemaContact = z.object().shape({
+//   fullname: zod
+//     .string()
+//     .trim('Vui lòng không nhập khoảng trắng !')
+//     .strict(true)
+//     .required('Vui lòng nhập họ tên !'),
+//   email: z.string().email('Vui nhập đúng định dạng !'),
+//   phone: yup
+//     .string()
+//     .matches(phoneRegExp, 'Số điện thoại không dúng định dạng !')
+//     .typeError('Vui lòng nhập số !')
+//     .required('Vui lòng nhập số điện thoại !'),
+//   problem: yup.string().required('Vui lòng chọn vấn đề!'),
+//   content: yup.string().required('Vui lòng nhập nội dung !')
+// })
 
 export const error = (element: any, errors: any) => {
   return (
