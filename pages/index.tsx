7ce1d113c208b8c17bf9afb55f88301f5b548720
Do Huu Huy <dohuuhuy2@gmail.com>
2021-11-22 20:28:25 +0700

    banner home

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,28 +4,24 @@ import HomeLayout from '@templates/Home'
 import { urlBanners } from '@utils/contants'
 import { fetcher } from '@utils/func'
 import { find } from 'lodash'
-import { useRouter } from 'next/router'
 import React from 'react'
 import { HomeCtl } from 'src/containers/home'
 import useSWR from 'swr'
 
 const HomePage = ({ data }: any) => {
-  const router = useRouter()
-  const {
-    query: { site },
-    pathname
-  } = router
-
   console.log('data :>> ', data)
 
   const { data: info, error } = useSWR(urlBanners, fetcher)
   if (error) return null
 
-  const key = site ? '/' + site : pathname
-  const getBanner = find(info, { key })
+  const getBannerHome = find(info, { key: '/' })
   return (
     <>
-      <BannerHome getBanner={getBanner} listFeature={[]} partnerId='medpro' />
+      <BannerHome
+        getBanner={getBannerHome}
+        listFeature={[]}
+        partnerId='medpro'
+      />
       <NewsEventCustom dataNewsAndEvent={data.newsAndEvent} />
     </>
   )
