5fa9645b814ffdbc99bb899635334f3d7556fc04
Do Huu Huy <dohuuhuy2@gmail.com>
2021-11-23 09:42:17 +0700

    v3

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,24 @@
 import '@assets/styles/app.less'
 import { wrapper } from '@store/rootStore'
+import { BackTop } from 'antd'
 import 'antd/dist/antd.css'
 import { DefaultSeo } from 'next-seo'
 import React, { Fragment } from 'react'
+import { useStore } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
 import SEO from 'support/next-seo.config'
 
 const MyApp = ({ Component, pageProps }: any) => {
   const Layout = Component?.Layout ?? Fragment
+  const store: any = useStore()
 
   return (
     <Layout>
       <DefaultSeo {...SEO} />
-      <Component {...pageProps} />
+      <PersistGate persistor={store.persistor}>
+        <Component {...pageProps} />
+      </PersistGate>
+      <BackTop />
     </Layout>
   )
 }
